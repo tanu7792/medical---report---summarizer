e9dd827dd047db910583e485efd78c5122ecdf49
tanu rathore <tanurathore7792@gmail.com>
2025-10-22 16:28:36 +0530

    Push full backend + frontend code

--- backend/apps/summaries/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ReportSummary
-
-class SummarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReportSummary
-        fields = "__all__"
-        read_only_fields = ("created_at",)
